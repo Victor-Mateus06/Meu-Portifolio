fda2d9d1dad755c06007b60d85f0bf0188728813
Victor-Mateus06 <165741108+Victor-Mateus06@users.noreply.github.com>
2024-04-07 16:27:20 -0300

    Add files via upload

--- src/js/index.js
+++ src/js/index.js
@@ -2,10 +2,31 @@ const botaoMostrarProjetos = document.querySelector('.btn-mostrar-projetos');
 const projetosInativos = document.querySelectorAll('.projeto:not(.ativo)');
 
 
+const botaoEsconderProjetos = document.querySelector('.btn-esconder-projetos');
+
+botaoEsconderProjetos.classList.add("remover");
+
 botaoMostrarProjetos.addEventListener('click', () => {
     mostrarMaisProjetos();
     esconderBotao();
+    botaoEsconderProjetos.classList.remove("remover");
 });
+
+botaoEsconderProjetos.addEventListener('click', () => {
+    esconderProjetos();
+    mostrarBotao();
+    botaoEsconderProjetos.classList.add("remover");
+})
+
+function mostrarBotao() {
+    botaoMostrarProjetos.classList.remove("remover");
+}
+
+function esconderProjetos() {
+    projetosInativos.forEach(projetoInativo => {
+        projetoInativo.classList.remove('ativo');
+    });
+}
 
 function esconderBotao() {
     botaoMostrarProjetos.classList.add("remover");
